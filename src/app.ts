c829a13648de7814d6d84fce215727fa7f79892c
Gabriel Macias <gabriel_pedernera@hotmail.com.ar>
2020-07-18 22:31:19 -0300

    adicionando algumas configuracoes iniciais

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+
+//settings
+const app = express();
+
+app.set('port', process.env.PORT || 2121);
+
+
+//middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//routes
+app.get('/', (req, res) =>{
+    res.send(`The API is running at http://localhost:${app.get('port')}`)
+});
+
+export default app;
